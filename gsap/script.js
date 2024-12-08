0536cb09331b96cafe554254002331ebf98677e5
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2024-12-08 18:08:32 +0500

    initial commits

---
+++ gsap/script.js
@@ -0,0 +1,25 @@
+gsap.to("#box",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    borderRadius:"50%",
+    scale:0.5,
+    repeat:-1,
+    yoyo:true,
+})
+gsap.from("#box2",{
+    x:500,
+    y:500,
+    duration:2,
+    delay:1,
+    backgroundColor:"beige",
+    borderRadius:"50%"
+})
+gsap.from("h1",{
+    opacity:0,
+    y:30,
+    duration:2,
+    delay:1,
+    stagger:1
+})
